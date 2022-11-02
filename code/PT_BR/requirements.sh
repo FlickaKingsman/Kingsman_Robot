4477f3450e5f87e80501ef9241ae30cd4016ac4c
kingsman <50754802+FlickaKingsman@users.noreply.github.com>
2022-11-02 04:11:50 -0300

    Add files via upload

---
+++ code/PT_BR/requirements.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo 
+echo -e "Lembre-se de estar no modo ROOT para instalar as bibliotecas necessárias"
+echo
+sleep 2s
+apt-get install boxes -y  2>/dev/null
+
+echo "Dependências Instaladas!"
+echo
